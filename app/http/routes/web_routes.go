5c31b2678cda8904a0479cff51e188561ef4ff49
Vinh Vo <vohuynhvinh@gmail.com>
2025-05-21 14:31:52 +0700

    Update session handling and clean-up unused configurations
    Refactor session management in web routes by introducing `SessionManipulation` middleware and refining authentication logic for specific routes. Removed obsolete JWT, AWS, and OAuth configurations from `.env.example` for improved clarity. Optimized `go.sum` by dropping redundant dependencies.

--- app/http/routes/web_routes.go
+++ app/http/routes/web_routes.go
@@ -10,15 +10,13 @@ import (
 
 // WebRoutes func for describe a group of Web page routes.
 func WebRoutes(r core.IFly) {
-	r.Use(middleware.SessionAuth(
-		"/",
-		"/login",
-	))
+	// Session Manipulation (NOTE: Put code top position in web router)
+	r.Use(middleware.SessionManipulation)
 
 	// Web Routers
 	r.GET("/", page.NewHomePage())
 
 	r.GET("/login", auth.NewLoginPage())
-	r.GET("/profile", user.NewProfilePage())
-	r.GET("/users", user.NewListPage())
+	r.GET("/profile", r.Apply(middleware.SessionAuthPage)(user.NewProfilePage()))
+	r.GET("/users", r.Apply(middleware.SessionAuthPage)(user.NewListPage()))
 }
